3e1c34d36e6e821ff4fffde54b75c1bfd6e82d37
alejandro gonzalez flores <agf_001@outlook.com>
2026-04-24 14:33:23 -0700

    Creando li en function main

--- index.js
+++ index.js
@@ -25,6 +25,40 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const li = document.createElement("li");
+  li.textContent = "Terminal";
+  li.classList.add("special");
+  const ul = document.querySelector("ul");
+  ul.appendChild(li);
+
+  const li2 = document.createElement("li");
+  li2.textContent = "Node";
+  li2.classList.add("special");
+  ul.appendChild(li2);
+
+  const li3 = document.createElement("li");
+  li3.textContent = "OOP";
+  li3.classList.add("special");
+  ul.appendChild(li3);
+
+  const li4 = document.createElement("li");
+  li4.textContent = "TypeScript";
+  li4.classList.add("special");
+  ul.appendChild(li4);
+
+  const li5 = document.createElement("li");
+  li5.textContent = "CSS";
+  li5.classList.add("special");
+  ul.appendChild(li5);
+
+  const li6 = document.createElement("li");
+  li6.textContent = "DOM";
+  li6.classList.add("special");
+  ul.appendChild(li6);
+    
+}
 
 main();
+
+
